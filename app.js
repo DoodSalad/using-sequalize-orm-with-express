0d19848f9e59d154044180616cd1d268a4cb6aa9
Riley Hays <haysrg@miamioh.edu>
2022-03-02 00:15:39 -0500

    Testing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const numner = 1;
 
 const routes = require('./routes/index');
 const articles = require('./routes/articles');
